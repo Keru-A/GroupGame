8e893991b583f514a70d6ec1b93b78ab70471557
Keru-A <alice.s.dowle@gmail.com>
2020-10-17 19:11:55 +1300

    Add the Start and First Room Methods

--- GroupGame/GroupGame/Program.cs
+++ GroupGame/GroupGame/Program.cs
@@ -6,7 +6,23 @@ namespace GroupGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //call the start screen
+            Start();
+            //call the first room which begins the game
+            FirstRoom();
+            Console.ReadLine();
+            
+        }
+
+        public static void Start()
+        {
+            //This is the method for the start screen
+            Console.WriteLine("Welcome to the game!");
+        }
+
+        public static void FirstRoom()
+        {
+            //This is the method for the first location of the game
         }
     }
 }
